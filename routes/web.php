ea719d1a2d01d298661d53a2b9f4adf285382cef
Najm Xhavier <xhavier817@gmail.com>
2022-11-30 23:34:17 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TableController;
+use App\Http\Controllers\DataTableController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/table',[TableController::class,'index'] );
+Route::get('/datatable',[DataTableController::class,'index'] );
